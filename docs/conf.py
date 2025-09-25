fc2b6de6013fe35307ae9a9e466ec60a531a06e1
Massimo Andreatta <massimo.andreatta@unil.ch>
2025-09-25 16:51:54 +0200

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@ from importlib.metadata import metadata
 from pathlib import Path
 
 HERE = Path(__file__).parent
-sys.path.insert(0, str(HERE / "extensions"))
-sys.path.insert(0, os.path.abspath(os.path.join('..')))
+sys.path.insert(0, str(HERE / "src" / "pyucell"))
+sys.path.insert(0, str(HERE))
 
 
 # -- Project information -----------------------------------------------------
