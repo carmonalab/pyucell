fee966cd41bf5c28b4abb23723326a2100788bbc
mass-a <massimo.andreatta@unige.ch>
2025-09-29 18:30:16 +0200

    fix global exports

--- src/pyucell/__init__.py
+++ src/pyucell/__init__.py
@@ -4,5 +4,5 @@ from .knn import smooth_knn_scores
 from .ranks import get_rankings
 from .scoring import compute_ucell_scores
 
-__all__ = ["get_rankings", "compute_ucell_scoressmooth_knn_scores"]
+__all__ = ["get_rankings", "compute_ucell_scores", "smooth_knn_scores"]
 __version__ = version("pyucell")
